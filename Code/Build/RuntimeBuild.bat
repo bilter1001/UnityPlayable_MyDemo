aa3fbab4a0e296bab7d01e459bd2cffe4246d32c
shenzhixiong_313322 <shenzhixiong_313322>
2021-04-26 17:56:40 +0800

    增加生成pdb文件的cmd命令

--- Code/Build/RuntimeBuild.bat
+++ Code/Build/RuntimeBuild.bat
@@ -2,13 +2,16 @@
 set CompileType=%1
 set SourceDir=%2
 set SourcePluginFileName=%3
-set SolutionName=%4
 set BatPath=%~dp0
-set ProjectTargetDir=%BatPath%..\..\Playable_MyDemo\Assets\Plugins\%SolutionName%\
+set ProjectTargetDir=%BatPath%..\..\Project\Assets\Plugins\AIDLL\
 set SourcePluginFilePath=%SourceDir%%SourcePluginFileName%
 set ProjectTargetPluginFilePath=%ProjectTargetDir%%SourcePluginFileName%
 set SourcePdbFilePath=%SourcePluginFilePath%.pdb
 set TargetPdbFilePath=%ProjectTargetDir%%SourcePluginFileName%.pdb
+:: Rar路径
+set MonoPath="C:\Program Files\Unity\Hub\Editor\2019.4.1f1\Editor\Data\MonoBleedingEdge\bin\mono.exe"
 
 if "%CompileType%"=="Debug" (echo f | xcopy /y "%SourcePdbFilePath%" "%TargetPdbFilePath%" ) else (del /q "%TargetPdbFilePath%")
 echo f | xcopy /y "%SourcePluginFilePath%.dll" "%ProjectTargetPluginFilePath%.dll" 
+
+%MonoPath% "%BatPath%pdb2mdb.exe" "%ProjectTargetPluginFilePath%.dll"
